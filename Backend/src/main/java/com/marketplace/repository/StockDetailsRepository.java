025617a51a8fb8534172dfcccf85259a1c4f9cb4
Rajaveer Patil <rajveer.patil.rr@gmail.com>
2026-02-17 16:25:09 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/marketplace/repository/StockDetailsRepository.java
@@ -0,0 +1,12 @@
+package com.marketplace.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.lang.String;
+
+import com.marketplace.pojos.StockDetails;
+
+@Repository
+public interface StockDetailsRepository extends JpaRepository<StockDetails, String>{
+	public StockDetails findByStockItem(String name);
+}
